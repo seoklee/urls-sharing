65c3c41e7986bd77b9c33305d29eb3f6e797f139
Tyler Massey <tylermassey6@gmail.com>
2016-01-03 15:58:15 -0600

    Added run script and stylesheets. Updated templates a bit.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source venv/bin/activate
+python surls/app.py
+deactivate
--- surls/app.py
+++ surls/app.py
@@ -1,6 +1,6 @@
 import sqlite3, random, string
 from flask import Flask, request, session, g, redirect, \
-	abort, render_template, flash
+    abort, render_template, flash
 from contextlib import closing
 
 # config
@@ -16,57 +16,62 @@ DEV_URL = 'http://127.0.0.1:5000'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 # database
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
 
 def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 
 def gen_url(length):
-	while True:
-		url = ''.join(random.choice(string.ascii_lowercase + string.digits) \
-			for _ in range(length))
-		db_url = g.db.execute('select * from entries where url = ?', [url])
-		entries = [dict(url=row[0], links=row[1], text=row[2]) for row in db_url.fetchall()]
-		print entries
-		if not entries:
-			return url
-
+    while True:
+        url = ''.join(random.choice(string.ascii_lowercase + string.digits) \
+                      for _ in range(length))
+        db_url = g.db.execute('select * from entries where url = ?', [url])
+        entries = [dict(url=row[0], links=row[1], text=row[2]) for row in db_url.fetchall()]
+        print entries
+        if not entries:
+            return url
 
 
 @app.before_request
 def before_request():
-	g.db = connect_db()
+    g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
-	db = getattr(g, 'db', None)
-	if db is not None:
-		db.close()
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/create', methods=['POST'])
 def create():
-	url = gen_url(4)
-	g.db.execute('insert into entries (url, links, text) values (?, ?, ?)',
-		[url, request.form['links'], request.form['text']])
-	g.db.commit()
-	return '{}/{}'.format(DEV_URL,url)
+    url = gen_url(4)
+    g.db.execute('insert into entries (url, links, text) values (?, ?, ?)',
+                 [url, request.form['links'], request.form['text']])
+    g.db.commit()
+    return '{}/{}'.format(DEV_URL, url)
 
 
 @app.route('/<url>')
 def urls(url=None):
-	result = g.db.execute('select * from entries where url = ?', [url])
-	entries = [dict(url=row[0], links=row[1], text=row[2]) for row in result.fetchall()]
-	return render_template('urls.html', entries=entries)
+    result = g.db.execute('select * from entries where url = ?', [url])
+    entries = [dict(url=row[0], links=row[1], text=row[2]) for row in result.fetchall()]
+    return render_template('urls.html', entries=entries)
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
