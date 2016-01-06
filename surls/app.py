8a189e6294c5ac744e7dd94e21a1cb4f08316732
Tyler Massey <tylermassey6@gmail.com>
2016-01-05 21:06:17 -0600

    Switched database from Sqlite3 to MongoDB. I also refactored to accomodate the change.
    I also split some of the code into more files. There is now a config.py and models.py.
    
    Updated requirements.txt as well.

--- surls/app.py
+++ surls/app.py
@@ -1,43 +1,29 @@
-import sqlite3, random, string
 from flask import Flask, request, session, g, redirect, \
     abort, render_template, flash
-from contextlib import closing
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongoengine import DoesNotExist
 from urlparse import urlparse
-
-# config
-DATABASE = '/tmp/surl.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'defult'
+# need to fix this to import only one model
+from models import *
+import random
+import string
 
 DEV_URL = 'http://127.0.0.1:5000'
 
 # creating the app
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object('config')
 
-
-# database
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+db = MongoEngine(app)
 
 
 def gen_url(length):
     while True:
         url = ''.join(random.choice(string.ascii_lowercase + string.digits) \
                       for _ in range(length))
-        db_url = g.db.execute('select * from entries where url = ?', [url])
-        entries = [dict(url=row[0], links=row[1], text=row[2]) for row in db_url.fetchall()]
-        print entries
-        if not entries:
+        try:
+            LinkEntry.objects.get(token=url)
+        except DoesNotExist:
             return url
 
 
@@ -48,18 +34,6 @@ def check_and_fix_http(url):
         url = "http://" + url
     print url
     return url
-
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
 
 
 @app.route('/')
@@ -73,20 +47,20 @@ def create():
     links = [x.strip() for x in request.form['links'].split(',')]
     for i, link in enumerate(links):
         links[i] = check_and_fix_http(link)
-    links_str = ','.join(links)
-    g.db.execute('insert into entries (url, links, text) values (?, ?, ?)',
-                 [url, links_str, request.form['text']])
-    g.db.commit()
+    link_entry = LinkEntry(
+        token=url,
+        links=links,
+        description=request.form['text']
+    )
+    link_entry.save()
     url_link = '{}/u/{}'.format(DEV_URL, url)
     return render_template('create.html', url_link=url_link)
 
 
 @app.route('/u/<url>')
 def urls(url=None):
-    result = g.db.execute('select * from entries where url = ?', [url])
-    entries = [dict(url=row[0], links=row[1], text=row[2]) for row in result.fetchall()]
-    entries[0]['links'] = entries[0]['links'].split(',')
-    return render_template('urls.html', entries=entries)
+    entry = LinkEntry.objects.get(token=url)
+    return render_template('urls.html', entry=entry)
 
 
 if __name__ == '__main__':
---
+++ surls/config.py
@@ -0,0 +1,8 @@
+# flask
+SECRET_KEY = 'secret_key'  # change this later?
+DEBUG = True
+
+# local mongodb
+MONGODB_SETTINGS = {
+    'DB': "surls"
+}
