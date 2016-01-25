0ca980a772d0eca17971fb7b3cf439a12cc04f41
Tyler Massey <tylermassey6@gmail.com>
2016-01-25 14:23:30 -0600

    also added styles for mobile devices and some js to automatically select the created url

--- surls/static/urls.js
+++ surls/static/urls.js
@@ -1,8 +1,16 @@
 (function () {
+    selectText();
+    var a = document.getElementsByClassName("content-url")[0];
+    a.addEventListener("click", selectText);
+
+})();
+
+
+function selectText() {
     var a = document.getElementsByClassName("content-url")[0];
     var selection = window.getSelection();
     var range = document.createRange();
     range.selectNodeContents(a);
     selection.removeAllRanges();
     selection.addRange(range);
-})();
+}
